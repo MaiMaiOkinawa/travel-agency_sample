6be4e5c1554da265497cc9690193ae9fef11b4af
Mai Uchida <maiuchidakishaba@gmail.com>
2023-06-05 12:14:35 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const navbar = document.querySelector(".header .flex .navbar");
+const hamburgerBtn = document.querySelector("#menu-btn");
+const guests = document.querySelectorAll('input[type="number"]');
+
+hamburgerBtn.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+guests.forEach((guest) => {
+  quest.oninput = () => {
+    if (guest.value.length > guest.maxLength)
+      guest.value = guest.value.slice(0, guest.maxLength);
+  };
+});
+
+AOS.init({
+  duration: 400,
+  delay: 200,
+});
